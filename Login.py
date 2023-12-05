7bd8a81d15cc11d47abc4a4b4f26c373425cd9c4
Tahashaikh <taha.tariq.shaikh@gmail.com>
2023-12-05 15:15:02 +0500

    done

--- Login.py
+++ Login.py
@@ -1,33 +1,17 @@
-from typing import re
+import re
 
 from playwright.sync_api import Page, expect
 
 
 
-def test_example(page: Page) -> None:
+def test_1example(page: Page) -> None:
     page.goto("https://www.saucedemo.com/")
-    page.locator("[data-test=\"username\"]").click()
-    page.locator("[data-test=\"username\"]").fill("visual_user")
-    page.locator("[data-test=\"username\"]").press("Tab")
-    page.locator("[data-test=\"password\"]").fill("secret_sauce")
-    page.locator("[data-test=\"login-button\"]").click()
-    page.locator("div").filter(has_text=re.compile(r"^Swag Labs$")).first.click()
+    expect(page).to_have_title(re.compile("Swag Labs"))
 
 
 
 
 
 
-def test_example(page: Page) -> None:
-    page.goto("https://www.saucedemo.com/")
-    page.locator("[data-test=\"username\"]").click()
-    page.locator("[data-test=\"username\"]").fill("standard_user")
-    page.locator("[data-test=\"username\"]").press("Tab")
-    page.locator("[data-test=\"password\"]").fill("secret_sauce")
-    page.locator("[data-test=\"login-button\"]").click()
-    page.locator("[data-test=\"add-to-cart-sauce-labs-backpack\"]").click()
-    page.locator("[data-test=\"add-to-cart-sauce-labs-bolt-t-shirt\"]").click()
-    page.locator("[data-test=\"add-to-cart-sauce-labs-fleece-jacket\"]").click()
-    page.locator("[data-test=\"add-to-cart-sauce-labs-bike-light\"]").click()
-    page.get_by_role("button", name="Open Menu").click()
-    page.get_by_role("link", name="Logout").click()
+
+
