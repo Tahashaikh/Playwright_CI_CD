bf3240e9fe9d20a6143435cc4fc690f3ebe14e54
Tahashaikh <taha.tariq.shaikh@gmail.com>
2023-12-05 15:18:22 +0500

    done

--- Login.py
+++ Login.py
@@ -5,6 +5,7 @@ from playwright.sync_api import Page, expect
 
 
 def test_1example(page: Page) -> None:
+    page.wait_for_timeout(3000)
     page.goto("https://www.saucedemo.com/")
     expect(page).to_have_title(re.compile("Swag Labs"))
 
