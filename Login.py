b846a72b692bec955e32a016fda2c8281b9a025b
Tahashaikh <taha.tariq.shaikh@gmail.com>
2023-12-14 12:59:40 +0500

    done

--- Login.py
+++ Login.py
@@ -9,6 +9,11 @@ def test_1example(page: Page) -> None:
     page.goto("https://www.saucedemo.com/")
     expect(page).to_have_title(re.compile("Swag Labs"))
 
+def test_2example(page: Page) -> None:
+    page.wait_for_timeout(3000)
+    page.goto("https://www.saucedemo.com/")
+    expect(page).to_have_title(re.compile("Swag Labs"))
+
 
 
 
